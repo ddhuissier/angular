bc784b9ec4837df301c0b360567d202ec3e8bfd7
ddhuissier <ddhuissier@gmail.com>
2021-10-04 13:03:42 +0200

    Starter app 12.2.0 (struct, material, bootstrap, fontawesome)

---
+++ starter-app/projects/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TagListComponent } from './shared/tag-list/tag-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'taglist',
+    component: TagListComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
